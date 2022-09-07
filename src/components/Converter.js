3e4d05a798c656af4f2be3bf143e030f3e86ac8b
BenBasic <benjaminmbasic@gmail.com>
2022-09-06 21:16:45 -0400

    Created IngredientList component and added some ingredients with their densitys included, began work on form which will use array data. Will continue building this as a front end app and implement a back end database if there are features which make sense to use a back end for

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
+import IngredientList from './IngredientList';
 
 export default function Converter() {
+
+    // Assigning the initial calcState to empty values, these will change depending on user input for the form
+    const [calcState, setCalcState] = useState({
+        amount: 1,
+        startUnit: "Cups (US)",
+        ingredient: "",
+        endUnit: "Grams",
+    });
+
+    // State for error message which will display on the form if user doesn't use correct input, no value by default
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const { amount, startUnit, ingredient, endUnit } = calcState;
+
+    // Array of ingredients and their density's from the IngredientList component
+    let infoList = IngredientList();
+
+    console.log(infoList)
+
 
     /* Originally used 236.588236 for cup but noticed results werent lining up to other calculators until
     using a value of 236.58823648491, this seems to be the correct value but will continue to look into it
@@ -32,7 +52,7 @@ export default function Converter() {
 
     // Pints to grams
     // grams = pints × 473.176473 × ingredient density
-    
+
     // Cups to grams
     // grams = cups × 236.588236 × ingredient density
 
@@ -69,13 +89,13 @@ export default function Converter() {
 
 
     function unitToWeight(unit, calculation, density) {
-        
+
         let result = unit * calculation * density;
         return result;
     };
 
     function weightToWeight(weight, weightCalc) {
-        
+
         let result = weight * weightCalc;
         return result;
     };
@@ -85,45 +105,66 @@ export default function Converter() {
     console.log(unitToWeight(1, cupGramCalc, 1.38));
     console.log(weightToWeight(testMeasure, gToKg));
 
-    return(
-        <div>
-            <h1>Gallons to Grams</h1>
-            <h2>{unitToWeight(1, gallonGramCalc, 1.38)}</h2>
+    return (
+        <>
+            <div className={`convertContainer`}>
 
-            <h1>Quarts to Grams</h1>
-            <h2>{unitToWeight(1, quartGramCalc, 1.38)}</h2>
+                <section className="contactContainer mt-md-5 mt-3">
+                    <h1 className='text-center formTitle'>Fun title</h1>
+                    <form id='conversion-form'>
+                        
+                        {errorMessage && (
+                            <div>
+                                <p className="errorAlert">{errorMessage}</p>
+                            </div>
+                        )}
+                        <div className="submitContainer">
+                            <button type="submit" className='submitButton'>Send</button>
+                        </div>
+                    </form>
+                </section>
+            </div>
 
-            <h1>Pints to Grams</h1>
-            <h2>{unitToWeight(1, pintGramCalc, 1.38)}</h2>
 
-            <h1>Cups to Grams</h1>
-            <h2>{unitToWeight(1, cupGramCalc, 1.38)}</h2>
+            <div>
+                <h1>Gallons to Grams</h1>
+                <h2>{unitToWeight(1, gallonGramCalc, 1.38)}</h2>
 
-            <h1>Fluid Ounces to Grams</h1>
-            <h2>{unitToWeight(1, flOzGramsCalc, 1.38)}</h2>
+                <h1>Quarts to Grams</h1>
+                <h2>{unitToWeight(1, quartGramCalc, 1.38)}</h2>
 
-            <h1>Liters to Grams</h1>
-            <h2>{unitToWeight(1, literGramCalc, 1.38)}</h2>
+                <h1>Pints to Grams</h1>
+                <h2>{unitToWeight(1, pintGramCalc, 1.38)}</h2>
 
-            <h1>Tablespoons to Grams</h1>
-            <h2>{unitToWeight(1, tablespGramCalc, 1.38)}</h2>
-            
-            <h1>Teaspoons to Grams</h1>
-            <h2>{unitToWeight(1, tspGramCalc, 1.38)}</h2>
+                <h1>Cups to Grams</h1>
+                <h2>{unitToWeight(1, cupGramCalc, 1.38)}</h2>
 
-            <h1>Milliliters to Grams</h1>
-            <h2>{unitToWeight(1, mlGramCalc, 1.38)}</h2>
+                <h1>Fluid Ounces to Grams</h1>
+                <h2>{unitToWeight(1, flOzGramsCalc, 1.38)}</h2>
 
-            <h4>-----------------------------</h4>
+                <h1>Liters to Grams</h1>
+                <h2>{unitToWeight(1, literGramCalc, 1.38)}</h2>
 
-            <h1>Grams to Ounces</h1>
-            <h2>{weightToWeight(testMeasure, gToOz)}</h2>
+                <h1>Tablespoons to Grams</h1>
+                <h2>{unitToWeight(1, tablespGramCalc, 1.38)}</h2>
 
-            <h1>Grams to Pounds</h1>
-            <h2>{weightToWeight(testMeasure, gToLb)}</h2>
+                <h1>Teaspoons to Grams</h1>
+                <h2>{unitToWeight(1, tspGramCalc, 1.38)}</h2>
 
-            <h1>Grams to Kilograms</h1>
-            <h2>{weightToWeight(testMeasure, gToKg)}</h2>
-        </div>
+                <h1>Milliliters to Grams</h1>
+                <h2>{unitToWeight(1, mlGramCalc, 1.38)}</h2>
+
+                <h4>-----------------------------</h4>
+
+                <h1>Grams to Ounces</h1>
+                <h2>{weightToWeight(testMeasure, gToOz)}</h2>
+
+                <h1>Grams to Pounds</h1>
+                <h2>{weightToWeight(testMeasure, gToLb)}</h2>
+
+                <h1>Grams to Kilograms</h1>
+                <h2>{weightToWeight(testMeasure, gToKg)}</h2>
+            </div>
+        </>
     )
 }
---
+++ src/components/IngredientList.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function List() {
+
+    const list = [
+        {name: "Bone and meat meal, loose", density: 0.72},
+        {name: "Bone meal, loose", density: 0.80-0.96},
+        {name: "Cow intestine, boiled", density: 0.58},
+        {name: "Cow, lean, no bone, raw", density: 0.96},
+        {name: "Cow, intestine, raw", density: 0.93},
+        {name: "Goat, intestine, boiled", density: 0.56},
+        {name: "Goat, intestine, raw", density: 0.83},
+        {name: "Pork, fatty, with bone, boiled", density: 0.63},
+        {name: "Pork, fatty, with bone, raw", density: 0.97},
+        {name: "Pork, medium, with bone, boiled", density: 0.7},
+        {name: "Pork, medium, with bone, raw", density: 0.93},
+
+        //Sweets	
+
+        {name: "Glucolin, glucose", density: 0.4},
+        {name: "Jam", density: 1.333},
+        {name: "Jam, sweetened", density: 1.43},
+        {name: "Jelly", density: 1.245},
+        {name: "Nutella", density: 1.26},
+        {name: "Sucrose", density: 0.85},
+        {name: "Sucrose Octoacetate", density: 0.53},
+        {name: "Sugar", density: 0.95},
+        {name: "Sugar, dextrose", density: 0.62},
+        {name: "Sugar, dextrose", density: 0.58},
+        {name: "Sugar, granulated", density: 0.7},
+        {name: "Sugar, powdered", density: 0.56},
+        {name: "Sugar, white", density: 0.88},
+
+        //Syrup, blackcurrant, undiluted
+
+        {name: "Syrup, corn", density: 1.38},
+        {name: "Syrup, corn, light", density: 1.40},
+        {name: "Syrup, fruit, blackberry", density: 1.34},
+        {name: "Syrup, maple", density: 1.32},
+        {name: "Syrup, pancake", density: 1.312},
+
+        //Miscellaneous foods	
+
+        {name: "Baking powder", density: 0.9},
+        {name: "Gelatin", density: 0.72},
+        {name: "Monosodium glutamate", density: 1.62},
+        {name: "Mustard powdered", density: 0.26},
+        {name: "Protein supplement", density: 0.54},
+        {name: "Salad dressing", density: 1.1},
+
+    ]
+    return {list};
+}
